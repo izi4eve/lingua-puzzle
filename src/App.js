78e3e32982fc5c7939d45bcb21295b426e9e5a65
izi4ever <mynotedata@gmail.com>
2024-11-05 11:44:30 +0100

    .no-select, no ContextMenu

--- src/App.js
+++ src/App.js
@@ -71,7 +71,10 @@ const App = () => {
   }, [i18n]);
 
   return (
-    <div className="container">
+    <div 
+      className="container no-select"
+      onContextMenu={(e) => e.preventDefault()} // Disable the context menu
+    >
       <div className="row justify-content-center">
         <div className="col-12 col-md-10 col-lg-8 col-xl-7 overflow-hidden">
 
